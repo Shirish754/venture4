b52deaa9260f101c2d6eabe4b8761ac2b3b1d729
Shirish Pokhrel <shpokh455@gmail.com>
2022-12-13 08:02:40 +0545

    api

--- src/Components/Home/HomeTopp.js
+++ src/Components/Home/HomeTopp.js
@@ -26,28 +26,63 @@ function HomeTop(props) {
             </div>
 
             <div>
-                <Dropdown >
-                    <div className="d-flex col m-4">
-                        <Dropdown.Toggle variant="light" id="dropdown-basic" className="col-3 m-1" >
-                            छान्नुहोश
+                <div className="d-flex justify-content-between col m-4 ">
+
+                    <Dropdown className="col-3 me-2 mt-2" >
+                        <Dropdown.Toggle variant="light" id="dropdown-basic" className="col-12"  >
+                            छान्नुहोश0
                         </Dropdown.Toggle>
-                        <Dropdown.Toggle variant="light" id="dropdown-basic" className="col-3 m-1">
-                            छान्नुहोश
-                        </Dropdown.Toggle>
-                        <Dropdown.Toggle variant="light" id="dropdown-basic" className="col-3 m-1">
-                            छान्नुहोश
-                        </Dropdown.Toggle>
-                        <Dropdown.Toggle variant="light" id="dropdown-basic" className="col-3 m-1">
-                            छान्नुहोश
-                        </Dropdown.Toggle>
-                    </div>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Action0</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action0</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else0</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
 
 
-                </Dropdown>
+                    <Dropdown className="col-3 m-2">
+                        <Dropdown.Toggle variant="light" id="dropdown-basic" className="col-12">
+                            छान्नुहोश1
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Action1</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action1</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else1</Dropdown.Item>
+                        </Dropdown.Menu>
 
+                    </Dropdown>
+
+
+                    <Dropdown className="col-3 m-2">
+                        <Dropdown.Toggle variant="light" id="dropdown-basic" className="col-12">
+                            छान्नुहोश2
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Action2</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action2</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else2</Dropdown.Item>
+                        </Dropdown.Menu>
+
+                    </Dropdown>
+
+
+                    <Dropdown className="col-3 m-2 ">
+
+                        <Dropdown.Toggle variant="light" id="dropdown-basic" className="col-12">
+                            छान्नुहोश3
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            <Dropdown.Item href="#/action-1">Action3</Dropdown.Item>
+                            <Dropdown.Item href="#/action-2">Another action3</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else3</Dropdown.Item>
+                        </Dropdown.Menu>
+                    </Dropdown>
+
+
+                </div>
             </div>
 
-        </div>
+        </div >
     );
 }
 export default HomeTop;
--- src/Components/Home/PositionHolders.js
+++ src/Components/Home/PositionHolders.js
@@ -1,18 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Photos from "../images/Photo.png"
+import { baseUrl } from "../baseUrl";
 
 function PHolders(props) {
+
+    const [users, setUsers] = useState([]);
+
+
+    const fetchDummyAPI = async () => {
+        await fetch(baseUrl + 'users')
+            .then((res) => res.json())
+            .then((res) => {
+                setUsers(res);
+                console.log(res);
+            })
+            .catch((e) => alert("Something went wrong"))
+    }
+
+    useEffect(() => {
+        fetchDummyAPI();
+    }, [])
+
     return (
         <div className=" px-4 py-2 bg-white m-4">
             <p className="fs-4 mt-4"><b>वडागत पदाधिकारी</b></p>
             <div className="col-12 bg-white d-flex flex-wrap justify-content-between mb-4" type="button">
 
-                {Array.from({ length: 5 }).map((_, index) => (
-                    <div className=" col-2 border d-flex flex-column align-items-center p-4 m-2 ">
+                {users?.map((user, index) => (
+                    <div className=" col-2 border d-flex flex-column align-items-center p-4 m-2 " key={index}>
                         <img className="d-flex justify-content-center align-items-center m-4" src={Photos} style={{ maxHeight: 120, maxWidth: 120, objectFit: "cover" }} />
                         <div className="d-flex justify-content-center align-items-center flex-column">
-                            <p className="" styles={{ padding: 0, margin: 0 }}><b>वडागत पदाधिकारी</b></p>
-                            <p styles={{ padding: 0, margin: 0, color: "#5C6C74" }}>वडा अद्धक्च्या</p>
+                            <p className="" styles={{ padding: 0, margin: 0 }}><b>{user.email}</b></p>
+                            <p styles={{ padding: 0, margin: 0, color: "#5C6C74" }}>{user.name}</p>
                         </div>
                     </div>
                 ))}
